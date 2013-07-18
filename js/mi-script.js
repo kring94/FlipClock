5c462ed60bc8f62cf7da71d727ef57034f69093d
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2013-07-17 21:22:20 -0500

    Mi temporizador: Días, horas, minutos, segundos.

---
+++ js/mi-script.js
@@ -0,0 +1,18 @@
+$(document).on('ready', iniciarTemporizador);
+
+function iniciarTemporizador() {
+
+	var fechaFinal = new Date(2013, 07, 30, 00, 00, 00);
+	var fechaHoy = new Date();
+
+	var diferenciaMilisegundos = fechaFinal.getTime() - fechaHoy.getTime();
+	var diferenciaSegundos = (diferenciaMilisegundos/1000);
+
+	var clock = $("#mi-reloj").FlipClock({
+	clockFace: 'DailyCounter'
+	});
+
+	clock.setTime(diferenciaSegundos);
+	clock.setCountdown(true);
+
+}
