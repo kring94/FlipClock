420fcafbf3f26204cb941cbed1f3fc5459aa30bf
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2016-03-27 12:37:16 -0500

    Se toma la hora del cliente.
    Se comentaron las líneas que toman la hora del servidor.
    Se quitó el ancho predefinido con CSS para que abarque el 100%.
    
    Changes committed:
    	modified:   css/mis-estilos.css
    	modified:   js/mi-script.js

--- js/mi-script.js
+++ js/mi-script.js
@@ -1,19 +1,29 @@
 $(document).on('ready', function(){
 
+// Iniciar tomando la hora del cliente
+iniciarTemporizador();
+
+// Iniciar tomando la hora del servidor
+/*
     var dominio = 'http://localhost:8888';
 
     var servidor = $.get(dominio +'/hora.php', function(data){
         horaServidor = data;
         iniciarTemporizador(horaServidor);
     });
+*/
 
 });
 
 function iniciarTemporizador(horaServidor) {
 
-	var fechaFinal = new Date(2015, 01, 08, 00, 00, 00);
-    //var fechaHoy = new Date();
+	var fechaFinal = new Date(2016, 11, 31, 23, 59, 59);
+    var fechaHoy = new Date();
+
+    // Para obtener la hora del servidor
+    /*
     var fechaHoy = new Date(horaServidor);
+    */
 
 	var diferenciaMilisegundos = fechaFinal.getTime() - fechaHoy.getTime();
 	var diferenciaSegundos = (diferenciaMilisegundos/1000);
@@ -31,7 +41,7 @@ function iniciarTemporizador(horaServidor) {
     function redireccionar()
     {
         if (clock.getTime() <= 0) {
-            window.location.replace('https://twitter.com/danielromeroauk');
+            window.location.replace('http://adsiar.com');
         }
     }
 
